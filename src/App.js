e094e2a3a1dc403ec20303656fa8f2b97869d034
shartechs <shartechs@gmail.com>
2021-11-01 00:50:11 +0400

    test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  // name = () => "Mary Jane"
 
   render() {
+    const name = "Mary Jane"
+    const loading = false
+    const showName = true
+
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>My App</h1>
+        {loading ? <h2>Loading...</h2> : <h2>Hello {showName && name}</h2>}
       </div>
     );
   }
